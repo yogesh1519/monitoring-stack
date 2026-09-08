aff4bac0f1e170f1c97ce78caa1c6a7bda501f30
Yogesh <yogeshtashildar10@gmail.com>
2026-09-08 11:56:04 +0530

    Initial commit: Prometheus + Grafana monitoring stack

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+const PORT = 3000;
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+const httpRequestCounter = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests',
+  labelNames: ['method', 'route', 'status']
+});
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    httpRequestCounter.inc({ method: req.method, route: req.path, status: res.statusCode });
+  });
+  next();
+});
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok', message: 'App is running!' });
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
